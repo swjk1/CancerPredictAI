dc2c8091f1cbd9ea6c4de604dd28b687216ac6c3
isabella-ll <isali2007le.er@gmail.com>
2024-11-16 13:42:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import panda as pd
+import plotly.express as px
 
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
-st.title("🎈 project cancer risk")
+from scipy.special import inv_boxcox
 
-st.subheader('Raw Data')
+def run():
+  st.title("🎈 project cancer risk")
+
+  st.subheader('Raw Data')
