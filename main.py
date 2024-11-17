6bc0b9b266c78cf29c15f3a0798fdfec82a09f29
swjk1 <leonzh1018@gmail.com>
2024-11-17 18:29:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder 
 import numpy as np
 
 st.title("🎈 project cancer risk")
