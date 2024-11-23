02e3dc7d0152c8e5b651e143c3a7bbb8c158923b
SherryNi13 <sherryniamc@gmail.com>
2024-11-23 13:05:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 import numpy as np
 
 # Load the dataset
