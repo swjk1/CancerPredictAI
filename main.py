6a441022f6a0f0ad21ce1291df45b921faa60243
SherryNi13 <sherryniamc@gmail.com>
2024-11-27 12:38:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -32,9 +32,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random
 model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
 
-# Streamlit app layout
-st.title("Cancer Risk Assessment Model")
-
 # Sidebar: Input Patient Data
 st.sidebar.header("Input Patient Data")
 
